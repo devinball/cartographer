5194379b947282c0c45597b2229c703cb13136cf
Devin <devinlynnball@gmail.com>
2024-01-13 00:57:08 -0700

    Imporved Performance

--- cartographer.py
+++ cartographer.py
@@ -1,14 +1,21 @@
-from PIL import Image
 import numpy as np
+import numba
+from PIL import Image
 
 # Color based on slope and elevation
 # Could do it seperate, but probably best to do it in the loop we already have
 # we could have a main loop where get call is_boundary_point and get_color_from_slope or something
 
-fp : str = "./heightmaps"
-step : int = 10
-max_pixel_value : int = 100
-render_scale : float = 0.5
+# So what i have learned is that numba can make python code stupid fast
+# like really
+
+# so today i have learned that computers are stupid fast
+# like dang
+
+fp : str = "./heightmaps/render(2).png"
+step : int = 5
+max_pixel_value : int = 255
+render_scale : float = 1
 
 line_color : tuple[int, int, int] = (31, 16, 0)
 start_color : tuple[int, int, int] = (43, 23, 0)
@@ -19,9 +26,11 @@ heightmap = heightmap.resize((int(heightmap.width * render_scale), int(heightmap
 
 width, height = heightmap.width, heightmap.height
 
+@numba.njit
 def interpolate(a : float, b : float, t : float):
     return a * (1.0 - t) + (b * t)
 
+@numba.njit
 def color_ramp(a : tuple[int, int, int], b : tuple[int, int, int], t : float) -> tuple[int, int, int]:
     return (
         int(interpolate(a[0], b[0], t)),
@@ -29,12 +38,16 @@ def color_ramp(a : tuple[int, int, int], b : tuple[int, int, int], t : float) ->
         int(interpolate(a[2], b[2], t)),
     )
 
-def get_point_in_bounds(img : Image.Image, x : int, y : int) -> int:
-    x = max(0, min(x, img.width-1)) 
-    y = max(0, min(y, img.height-1))
-    return img.getpixel((x, y))
+@numba.njit
+def get_point_in_bounds(img : np.ndarray, x : int, y : int) -> int:
+    img_width = img.shape[0]
+    img_height = img.shape[1]
+    x = max(0, min(x, img_width-1)) 
+    y = max(0, min(y, img_height-1))
+    return int(img[x, y])
 
-def get_neighbors(img : Image.Image, x : int, y : int) -> list[int]:
+@numba.njit
+def get_neighbors(img : np.ndarray, x : int, y : int) -> list[int]:
     return [
         get_point_in_bounds(img, x-1, y-1),
         get_point_in_bounds(img, x-1, y  ),
@@ -48,12 +61,21 @@ def get_neighbors(img : Image.Image, x : int, y : int) -> list[int]:
         get_point_in_bounds(img, x+1, y+1),
     ]
 
-def is_boundary_point(img : Image.Image, x : int, y : int) -> bool:
+@numba.njit
+def any_true(a : list[bool]) -> bool:
+    for i in a:
+        if i:
+            return True
+    return False
+
+@numba.njit
+def is_boundary_point(img : np.ndarray, x : int, y : int) -> bool:
         pix = get_point_in_bounds(img, x, y)
         neighbors = get_neighbors(img, x, y)
-        return any([pix < neighbor for neighbor in neighbors])
+        return any_true([pix < neighbor for neighbor in neighbors])
 
-def get_slope(img : Image.Image, x : int, y : int) -> int:
+@numba.njit
+def get_slope(img : np.ndarray, x : int, y : int) -> int:
     up    = get_point_in_bounds(img, x  , y+1)
     down  = get_point_in_bounds(img, x  , y-1)
     left  = get_point_in_bounds(img, x+1, y  )
@@ -64,30 +86,35 @@ def get_slope(img : Image.Image, x : int, y : int) -> int:
 
     return vertical
 
-def march_image(output : Image.Image, heightmap : Image.Image, stepped_heightmap : Image.Image):
+@numba.njit
+def march_image(output : np.ndarray, heightmap : np.ndarray, stepped_heightmap : np.ndarray) -> None:
     boundary_points : list[tuple[int, int]] = []
 
-    for x in range(width):
-        for y in range(height):
+
+    # it would make sense that x is in range of width, and y is in range of height
+    # it would make sense, wouldn't it
+    # i don't understand it
+    for x in range(height):
+        for y in range(width):
             #slope = get_slope(heightmap, x, y) * 10
             #color = (slope,slope,slope) #step_color(slope) #+ get_point_in_bounds(heightmap, x, y)) # 
             #output.putpixel((x,y), color)
 
             color = color_ramp(start_color, end_color, get_point_in_bounds(stepped_heightmap, x, y)/max_pixel_value)
 
-            output.putpixel((x,y), color)
+            output[x ,y] = color
 
             if is_boundary_point(stepped_heightmap, x, y):
                 boundary_points.append((x, y))
 
     for b in boundary_points:
-        output.putpixel(b, line_color)
+        output[b] = line_color
 
 def step_image(img : Image.Image) -> Image.Image:
     arr = np.array(img)
 
     nums = list(range(0, max_pixel_value, step))
-    arr = np.minimum(arr, nums[-1])
+    nums.append(max_pixel_value)
 
     for idx in range(len(nums)):
         i = max(0,idx-1)
@@ -95,12 +122,11 @@ def step_image(img : Image.Image) -> Image.Image:
 
     return Image.fromarray(arr)
 
-
 print("Creating Stepped Image...")
 stepped_heightmap = step_image(heightmap)
 
 print("Marching Image...")
-output = Image.new("RGB", (width, height))
-march_image(output, heightmap, stepped_heightmap)
+output = np.array(Image.new("RGB", (width, height)))
 
-output.show()
+march_image(output, np.array(heightmap), np.array(stepped_heightmap))
+Image.fromarray(output).show()
---
+++ run.sh
@@ -0,0 +1,3 @@
+mypyc cartographer.py
+python3 -c "import cartographer"
+rm cartographer.cpython-310-x86_64-linux-gnu.so
